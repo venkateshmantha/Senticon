77788cfc330c8c139f86cf6dc1b6bf8fad752a61
Venkatesh Mantha <Venkatesh Mantha>
2016-10-09 11:32:23 -0400

    changes

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var app = express();
 
 
 app.get('/', function(req, res){
-	res.sendStatus(200);
-    // res.sendFile(path.resolve('./index.html'));
+    res.sendFile(path.resolve('./index.html'));
 });
 
 app.get('/ajax/endpoint', function (req, res) {
